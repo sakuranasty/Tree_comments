34c67a458dc36f01932125df29e083dde69a4d60
apavlova <anastasiya.pavlova@emergn.com>
2021-12-24 03:03:40 +0300

    Small fixes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,4 @@
 import { Component } from '@angular/core';
-import { map, Observable } from 'rxjs';
-import { timer } from 'rxjs/internal/observable/timer';
-import { comments } from './data/comments';
-import { AppComment } from './types/app-comment.type';
 
 @Component({
   selector: 'app-root',
